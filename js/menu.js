edfee926f058c8e991c16feb4d65fe05fef3b024
mb8504 <mbergmarketing@gmail.com>
2023-12-21 16:21:29 -0500

    finished nav

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+const primaryNav = document.querySelector('.nav__ul');
+const navToggle = document.querySelector('.header__button');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible')
+    
+    if (visibility === 'false') {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else if (visibility === 'true') {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+});
+

b6f56458db69e5c7eb534c40b09140bdecf9712f
mb8504 <mbergmarketing@gmail.com>
2024-01-11 08:32:16 -0500

    cart count updated

--- js/product_click.js
+++ js/product_click.js
@@ -83,13 +83,17 @@ function addItemToCart() {
 // Function to save the cart to local storage
 function saveCartToLocalStorage() {
     localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
-    localStorage.setItem('cartCountElement', JSON.stringify(shoppingCart));
+    localStorage.setItem('cartCountElementMobile', JSON.stringify(shoppingCart));
+    localStorage.setItem('cartCountElementDesktop', JSON.stringify(shoppingCart));
 }
 
 // Function to update the cart count in the NAV
 function updateCartCount() {
-    let cartCountElement = document.getElementById('cartCount');
-    cartCountElement.textContent = shoppingCart.totalQuantity;
+    let cartCountElementMobile = document.getElementById('cartCountMobile');
+    let cartCountElementDesktop = document.getElementById('cartCountDesktop');
+
+    cartCountElementMobile.textContent = shoppingCart.totalQuantity;
+    cartCountElementDesktop.textContent = shoppingCart.totalQuantity;
 }
 
 // Load the cart from local storage when the page loads

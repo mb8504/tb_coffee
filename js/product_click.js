05172664cb3c195555e3670281621e5380e6793f
mb8504 <mbergmarketing@gmail.com>
2024-08-12 08:32:04 -0400

    updated shopping cart

--- js/product_click.js
+++ js/product_click.js
@@ -1,47 +1,94 @@
-// START BUTTON CLICK FUNCTIONALITY
-let quantityElement = document.getElementById('counter');
+const countQuantity = document.querySelector('.counter__quantity');
+const cardPrice = document.getElementById('cardPrice');
+let cartCountElementMobile = document.getElementById('cartCountMobile');
+let cartCountElementDesktop = document.getElementById('cartCountDesktop');
+const pricePerItem = 8;
 
-function increaseQuantity() {
-    let currentQuantity = parseInt(quantityElement.textContent, 10);
-    quantityElement.textContent = currentQuantity + 1;
-    updateTotalAmount();
+// START INCREASE AND DECREASE BUTTON CLICK
+const increaseQuantity = () => {
+    let currentPriceText = cardPrice.innerText;
+    let currentPrice = parseFloat(currentPriceText.replace('$', ''));
+    let newPrice = currentPrice + pricePerItem;
+
+    countQuantity.innerText++
+    cardPrice.innerText = '$' + newPrice.toFixed(2); 
 }
 
-function decreaseQuantity() {
-    let currentQuantity = parseInt(quantityElement.textContent, 10);
-    if (currentQuantity > 1) {
-        quantityElement.textContent = currentQuantity - 1;
-        updateTotalAmount();
+const decreaseQuantity = () => {
+    const pricePerItem = 8;
+    let currentPriceText = cardPrice.innerText;
+    let currentPrice = parseFloat(currentPriceText.replace('$', ''));
+    let newPrice = currentPrice - pricePerItem;
+
+    if (parseInt(countQuantity.innerHTML, 10) > 0) {
+        countQuantity.innerHTML--
+        cardPrice.innerText = '$' + newPrice.toFixed(2); 
     }
 }
 
-// END BUTTON CLICK FUNCTIONALITY
-
-// START PRICE CHANGE
-let totalAmountElement = document.getElementById('cardPrice');
-let originalPrice = 8.00;
-
-function updateTotalAmount() {
-    let currentQuantity = parseInt(quantityElement.textContent, 10);
-    let totalAmount = originalPrice * currentQuantity;
-    totalAmountElement.textContent = '$' + totalAmount.toFixed(2);
-}
-// END PRICE CHANGE
-
-
-// Assume you have a shopping cart object
+// START shoppingCart OBJECT
 let shoppingCart = {
     items: [],
+    totalPrice: 0,
     totalQuantity: 0,
-    totalPrice: 0.00,
 };
 
+// START ADD TO CART BUTTON 
+function addItemToCart() {
+    let productTitle = document.querySelector('.card__title').textContent;
+    let productImage = document.querySelector('.product__card__img').src;
 
-// START TABLE FOR CART ITEMS
+    let item = {
+        name: productTitle,
+        originalPrice: pricePerItem,
+        quantity: Number(countQuantity.innerText),
+        image: productImage,
+    };
 
+    // Add the item to the cart
+    shoppingCart.items.push(item);
+
+    shoppingCart.totalPrice += pricePerItem * countQuantity.innerText;
+    shoppingCart.totalQuantity += Number(countQuantity.innerText);
+
+    updateCartCount();
+
+    saveCartToLocalStorage('shoppingCart');
+}
+
+// Function to update the cart count in the NAV
+function updateCartCount() {
+    let cartCountElementMobile = document.getElementById('cartCountMobile');
+    let cartCountElementDesktop = document.getElementById('cartCountDesktop');
+
+    cartCountElementMobile.textContent = shoppingCart.totalQuantity;
+    cartCountElementDesktop.textContent = shoppingCart.totalQuantity;
+}
+
+// START SAVE TO LOCAL STORAGE FUNCTION
+function saveCartToLocalStorage() {
+    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
+    localStorage.setItem('cartCountElementMobile', JSON.stringify(shoppingCart));
+    localStorage.setItem('cartCountElementDesktop', JSON.stringify(shoppingCart));
+}
+
+// Function to load the cart from local storage when the page loads
+function loadCartFromLocalStorage() {
+    let savedCart = localStorage.getItem('shoppingCart');
+    if (savedCart) {
+        shoppingCart = JSON.parse(savedCart);
+        updateCartCount(); // Update the cart count
+        updateCartDisplay();
+    }
+}
+
+loadCartFromLocalStorage();
+
+
+// START CART PAGE
 function updateCartDisplay() {
     let cartContainer = document.getElementById('cartContainer');
-    cartContainer.innerHTML = ""; // Clear existing content
+    cartContainer.innerHTML = ""; 
 
     // Create table element
     let table = document.createElement('table');
@@ -66,7 +113,7 @@ function updateCartDisplay() {
 
     // Iterate through items and generate table rows
     shoppingCart.items.forEach(item => {
-        let totalPrice = item.originalPrice * item.quantity; // Calculate total price
+        let totalPrice = item.originalPrice * item.quantity; 
 
         let row = document.createElement('tr');
         row.classList.add('item__row');
@@ -88,75 +135,11 @@ function updateCartDisplay() {
     clearCartButton.style.display = shoppingCart.items.length > 0 ? 'block' : 'none';
 }
 
-// END TABLE FOR CART ITEMS
 
-
-
-
-// Function to load the cart from local storage when the page loads
-function loadCartFromLocalStorage() {
-    let savedCart = localStorage.getItem('shoppingCart');
-    if (savedCart) {
-        shoppingCart = JSON.parse(savedCart);
-        updateCartDisplay();
-        updateCartCount(); // Update the cart count when loading from local storage
-    }
-}
-
-// START ADD TO CART
-// Function to Add To Cart
-function addItemToCart() {
-    let currentQuantity = parseInt(quantityElement.textContent, 10);
-    let productTitle = document.querySelector('.card__title').textContent;
-    let productImage = document.querySelector('.product__card__img').src;
-
-    // item object with relevant information
-    let item = {
-        name: productTitle,
-        originalPrice: originalPrice,
-        quantity: currentQuantity,
-        image: productImage,
-    };
-
-    // Add the item to the cart
-    shoppingCart.items.push(item);
-
-    // Update total quantity and total price in the cart
-    shoppingCart.totalQuantity += currentQuantity;
-    shoppingCart.totalPrice += originalPrice * currentQuantity;
-
-    updateCartCount();
-
-    // Save the cart to local storage
-    saveCartToLocalStorage('shoppingCart');
-    
-}
-
-
-
-// Function to save the cart to local storage
-function saveCartToLocalStorage() {
-    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart));
-    localStorage.setItem('cartCountElementMobile', JSON.stringify(shoppingCart));
-    localStorage.setItem('cartCountElementDesktop', JSON.stringify(shoppingCart));
-}
-
-// Function to update the cart count in the NAV
-function updateCartCount() {
-    let cartCountElementMobile = document.getElementById('cartCountMobile');
-    let cartCountElementDesktop = document.getElementById('cartCountDesktop');
-
-    cartCountElementMobile.textContent = shoppingCart.totalQuantity;
-    cartCountElementDesktop.textContent = shoppingCart.totalQuantity;
-}
-
-// Load the cart from local storage when the page loads
-loadCartFromLocalStorage();
 
 
 // <------- START CART CLEAR CART  ------->
 
-// Function to clear the cart
 function clearCart() {
     // Reset the shoppingCart object
     shoppingCart = {
@@ -168,11 +151,12 @@ function clearCart() {
     // Update cart display and count
     updateCartDisplay();
     updateCartCount();
-
+    
     // Clear cart data from local storage
     localStorage.removeItem('shoppingCart');
     localStorage.removeItem('cartCountElementMobile');
     localStorage.removeItem('cartCountElementDesktop');
+
 }
 
 
